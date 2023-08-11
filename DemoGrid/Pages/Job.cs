46a1e928dbeb02677c97df0d0419457fd1140092
jayesh304 <jayesh.30patil@gmail.com>
2023-08-11 23:49:04 +0530

    pushing to local

---
+++ DemoGrid/Pages/Job.cs
@@ -0,0 +1,31 @@
+﻿public class CadJobDto
+{
+    public int? cadSoftwareId { get; set; }
+    public CadSoftware cadSoftware { get; set; }
+    public string errorMessage { get; set; }
+    public int? cadSoftwareAutomationJobPriority { get; set; }
+    public int? cadSoftwareAutomationJobStatus { get; set; }
+    public int? configurationId { get; set; }
+    public int? revisionId { get; set; }
+    public string tcsFileName { get; set; }
+    public string environment { get; set; }
+    public string storage { get; set; }
+    public DateTime? queuedOn { get; set; }
+    public DateTime? estimatedTDelivery { get; set; }
+    public int Id { get; set; }
+    public string createdBy { get; set; }
+    public DateTime? createdOn { get; set; }
+    public string modifiedBy { get; set; }
+    public DateTime? modifiedOn { get; set; }
+}
+
+public class CadSoftware
+{
+    public string name { get; set; }
+    public object cadServers { get; set; }
+    public int Id { get; set; }
+    public string createdBy { get; set; }
+    public DateTime createdOn { get; set; }
+    public object modifiedBy { get; set; }
+    public DateTime modifiedOn { get; set; }
+}
---
+++ DemoGrid/Pages/JobList.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace DemoGrid.Pages;
+
+public partial class JobList
+{
+    private List<CadJobDto> cadJobs=new();
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        var requestModel = new
+        {
+            userName = "vishal.banwat@scandinaviandigital.com",
+            fromdate = "2022-07-03T12:53:35.139Z",
+            todate = "2023-08-03T12:53:35.139Z"
+        };
+
+        var jsonContent = new StringContent(JsonSerializer.Serialize(requestModel), Encoding.UTF8, "application/json");
+
+        using (var httpResponse = await HttpClient.PostAsync("https://api.config2.com/api/Queue/ListOfAllJobsByUser", jsonContent))
+        {
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var result = await httpResponse.Content.ReadAsStringAsync();
+                cadJobs= JsonSerializer.Deserialize<List<CadJobDto>>(result);
+            }
+            else
+            {
+                cadJobs = new List<CadJobDto>();
+            }
+        }
+    }
+
+ 
+}
